d695574e2a9d264ebc2a8629d3f1099d07f810e8
olog91 <olog91@gmail.com>
2022-08-16 21:56:42 +0300

    add youtobe and start folders

---
+++ src/routes/youtobe/html_tags.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Layout from "../components/layout";
+
+
+export default function Faqs () {
+
+
+    return (
+
+        <>
+        <Layout>
+        <p>Faqs</p>
+        </Layout>
+        </>
+
+    )
+
+
+}
